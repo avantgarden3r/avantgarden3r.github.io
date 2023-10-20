3b5b787104fe88859f997e0a23b5901ecc53e15e
avantgarb4ge <112592431+avantgarden3r@users.noreply.github.com>
2023-10-20 06:38:46 -0700

    portfolio

---
+++ js/main.js
@@ -0,0 +1,42 @@
+
+// //navbar dropdown menu
+// function dropMenu() {
+//     document.getElementById("myDropdown").classList.toggle("show");
+//   }
+  
+  // Close the dropdown if the user clicks outside of it
+//   window.onclick = function(e) {
+//     if (!e.target.matches('.dropbtn')) {
+//     var myDropdown = document.getElementById("dropMenu");
+//       if (myDropdown.classList.contains('show')) {
+//         myDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+
+// class myHeader extends HTMLElement {
+//     connectedCallback() {
+//         this.innerHTML = `
+//         <header>
+//             <a class="active" href="#home">Home</a>
+//             <a href="./html/about.html">About</a>
+//             <a href="./html/projects.html">GIS Projects</a>
+//             <a href="./html/contact.html">Contact</a>
+//         </header>
+//         `
+//     }
+// }
+
+// customElements.define('my-header', myHeader)
+
+// class myFooter extends HTMLElement {
+//     connectedCallback() {
+//         this.innerHTML = `
+//         <footer>
+//             <p>Katherine Pak | Linkedin | GitHub</p>
+//         </footer>
+//         `
+//     }
+// }
+
+// customElements.define('my-footer', myFooter)
